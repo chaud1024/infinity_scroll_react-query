be5512bb6f761d17ea46e289f87bc0f0ed01c0a9
chaud1024 <chaud1024@gmail.com>
2023-02-05 15:52:18 +0900

    first commit

---
+++ src/Example1.js
@@ -0,0 +1,15 @@
+const Example1 = () => {
+  return (
+    <>
+      <h1 id="top">
+        &infin; Infinite Query &amp; Scroll
+        <br />
+        &infin; Ex. 1 - React only
+      </h1>
+      <p className="center">
+        <a href="#top">Back to Top</a>
+      </p>
+    </>
+  );
+};
+export default Example1;
