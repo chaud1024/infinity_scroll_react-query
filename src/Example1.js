a50d3eaf4f7e2b19f1338a4c8ef3ec21b8a41a17
chaud1024 <chaud1024@gmail.com>
2023-02-05 23:59:26 +0900

    add callback

--- src/Example1.js
+++ src/Example1.js
@@ -1,5 +1,6 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useCallback } from "react";
 import usePosts from "./hooks/usePosts";
+import Post from "./Post";
 
 const Example1 = () => {
   // state for pageNum
@@ -8,7 +9,26 @@ const Example1 = () => {
   // using the hook
   const { isLoading, isError, error, results, hasNextPage } = usePosts(pageNum); // pageNum state would be passed in
 
-  const lastPostRef = useRef();
+  // using useRef fot the last post
+  // change name to intObserver
+  const intObserver = useRef();
+  const lastPostRef = useCallback(
+    (post) => {
+      if (isLoading) return;
+
+      if (intObserver.current) intObserver.current.disconnect();
+
+      intObserver.current = new IntersectionObserver((posts) => {
+        if (posts[0].isIntersecting && hasNextPage) {
+          console.log("we are near the last post");
+          setPageNum((prev) => prev + 1);
+        }
+      });
+
+      if (post) intObserver.current.observe(post);
+    },
+    [isLoading, hasNextPage]
+  );
 
   // if there's an error, return the error message
   if (isError) return <p className="center">Error: {error.message}</p>;
@@ -18,6 +38,7 @@ const Example1 = () => {
     if (results.length === i + 1) {
       // that let us know the last element that we get from the results
       // console.log("last element");
+      // by using useRef, we can figure out what's the last result of results
       return <Post ref={lastPostRef} key={post.id} post={post} />;
     }
     return <Post key={post.id} post={post} />;
