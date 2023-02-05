a0e71c81850d8217bab67c74e08dc40a12e3779b
chaud1024 <chaud1024@gmail.com>
2023-02-05 22:52:14 +0900

    add usePosts hook

--- src/Example1.js
+++ src/Example1.js
@@ -1,4 +1,25 @@
+import { useState } from "react";
+import usePosts from "./hooks/usePosts";
+
 const Example1 = () => {
+  // state for pageNum
+  const [pageNum, setPageNum] = useState(1); // default value of 1
+
+  // using the hook
+  const { isLoading, isError, error, results, hasNextPage } = usePosts(pageNum); // pageNum state would be passed in
+
+  // if there's an error, return the error message
+  if (isError) return <p className="center">Error: {error.message}</p>;
+
+  // define content
+  const content = results.map((post, i) => {
+    if (results.length === i + 1) {
+      // that let us know the last element that we get from the results
+      console.log("last element");
+    }
+    return <Post key={post.id} post={post} />;
+  });
+
   return (
     <>
       <h1 id="top">
@@ -6,6 +27,8 @@ const Example1 = () => {
         <br />
         &infin; Ex. 1 - React only
       </h1>
+      {content}
+      {isLoading && <p className="center">Loading More Posts...</p>}
       <p className="center">
         <a href="#top">Back to Top</a>
       </p>
